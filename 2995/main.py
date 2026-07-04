cabebec5a17a3ecb65bacb671712a9d2d1a88b7c
69070196-ai <69070196@kmitl.ac.th>
2026-07-04 17:27:23 +0700

    student_id

--- 2995/main.py
+++ 2995/main.py
@@ -1,1 +1,6 @@
-print("hello world")
+student_id = input().strip()
+
+if len(student_id) == 8 and student_id.isdigit() and "1" in student_id[2] and "6" in student_id[3]:
+    print("yes")
+else:
+    print("no")
